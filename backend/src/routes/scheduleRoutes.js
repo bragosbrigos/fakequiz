3e37fcc8a151ca02ed6697dc73e5dd0494c8b3df
bragosbrigos <dannysike_@hotmail.com>
2026-05-25 11:50:29 -0400

    Update scheduleRoutes.js

--- backend/src/routes/scheduleRoutes.js
+++ backend/src/routes/scheduleRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getSchedule, syncMatches, updateImages, initDatabase } = require('../controllers/scheduleController');
 
-router.get('/api/schedule', getSchedule);
+router.get('/schedule', getSchedule);
 router.post('/schedule/sync', syncMatches);
 router.post('/update-images', updateImages);
 router.post('/init-db', initDatabase);
